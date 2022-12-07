c237d052a1849850e23f87de81eaad84a6f65eea
mcanam <mcanam910@gmail.com>
2022-12-08 00:37:45 +0700

    create spinbar component

---
+++ src/app/shared/components/spinbar/spinbar.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+      selector: 'app-spinbar',
+      templateUrl: './spinbar.component.html',
+      styleUrls: ['./spinbar.component.css']
+})
+export class SpinbarComponent implements OnInit {
+      constructor() { }
+
+      ngOnInit(): void { }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SpinbarComponent } from './components/spinbar/spinbar.component';
+
 import { MaterialsModule } from './modules/materials.module';
 import { IconsModule } from './modules/icons.module';
 
 @NgModule({
-      declarations: [],
+      declarations: [
+            SpinbarComponent
+      ],
       imports: [
             CommonModule,
             MaterialsModule,
             IconsModule
       ],
       exports: [
+            SpinbarComponent,
+            
             MaterialsModule,
             IconsModule
       ]
