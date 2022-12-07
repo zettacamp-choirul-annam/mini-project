8c11f03978e33b9a65d89fb8ae751ed2d7468fe2
mcanam <mcanam910@gmail.com>
2022-12-07 20:52:22 +0700

    add apollo angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
+import { GraphQLModule } from './graphql.module';
+
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
       declarations: [
@@ -11,8 +14,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
       ],
       imports: [
             BrowserModule,
+            BrowserAnimationsModule,
+            HttpClientModule,
             AppRoutingModule,
-            BrowserAnimationsModule
+            GraphQLModule,
       ],
       providers: [],
       bootstrap: [AppComponent]
---
+++ src/app/graphql.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
+import { ApolloClientOptions, InMemoryCache, ApolloLink } from '@apollo/client/core';
+import { HttpLink } from 'apollo-angular/http';
+import { HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+const uri = environment.api; // <-- add the URL of the GraphQL server here
+
+export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
+      const http = httpLink.create({ uri });
+
+      const middleware = new ApolloLink((operation, forward) => {
+            const token = localStorage.getItem('token') || null;
+
+            operation.setContext({
+                  headers: new HttpHeaders().set('Authorization', `${token}`)
+            });
+
+            return forward(operation);
+      });
+
+      return {
+            link: middleware.concat(http),
+            cache: new InMemoryCache()
+      }
+}
+
+@NgModule({
+      exports: [ApolloModule],
+      providers: [
+            {
+                  provide: APOLLO_OPTIONS,
+                  useFactory: createApollo,
+                  deps: [HttpLink],
+            },
+      ],
+})
+export class GraphQLModule { }
