1ed3d7a8c67e16c29b5b4df5d3a46a83ce00d30c
mcanam <mcanam910@gmail.com>
2022-12-12 01:13:50 +0700

    add ngx-translate library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppRoutingModule } from './app-routing.module';
 import { GraphQLModule } from './graphql.module';
@@ -12,6 +15,10 @@ import { ShellComponent } from './shell/shell.component';
 
 import { HeaderModule } from './shell/header/header.module';
 import { FooterModule } from './shell/footer/footer.module';
+
+export function HttpLoaderFactory(http: HttpClient) {
+      return new TranslateHttpLoader(http);
+}
 
 @NgModule({
       declarations: [
@@ -26,7 +33,16 @@ import { FooterModule } from './shell/footer/footer.module';
             GraphQLModule,
             SharedModule,
             HeaderModule,
-            FooterModule
+            FooterModule,
+
+            TranslateModule.forRoot({
+                  defaultLanguage: 'en',
+                  loader: {
+                        provide: TranslateLoader,
+                        useFactory: HttpLoaderFactory,
+                        deps: [HttpClient]
+                  }
+            }),
       ],
       providers: [],
       bootstrap: [AppComponent]
