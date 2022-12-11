bc0ced94ad8e4070d077f6d3d323a18480ab8601
mcanam <mcanam910@gmail.com>
2022-12-11 09:44:28 +0700

    update highlight section: add skeleton laoder & click handler

--- src/app/pages/home/sections/highlight/highlight.component.ts
+++ src/app/pages/home/sections/highlight/highlight.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { MenuService } from 'src/app/shared/services/menu.service';
+import { Router } from '@angular/router';
 
 @Component({
       selector: 'app-highlight',
@@ -11,20 +12,35 @@ export class HighlightComponent implements OnInit {
       sub!: Subscription;
       menus: any = [];
 
+      // state
+      isLoad : boolean = true;
+      isError: boolean = false;
+
       constructor(
-            private menuService: MenuService
+            private menuService: MenuService,
+            private router: Router
       ) { }
 
       ngOnInit(): void {
             this.sub = this.menuService.getHighlightMenu().subscribe({
-                  next: (result) => this.menus = result,
+                  next: (result) => {
+                        this.isLoad = false;
+                        this.menus = result;
+                  },
                   error: (error) => {
                         console.log(error);
+                        
+                        this.isLoad = false;
+                        this.isError = true;
                   }
             });
       }
 
       ngOnDestroy() {
             this.sub.unsubscribe();
+      }
+
+      goToMenu(id: string) {
+            this.router.navigate(['/menu'], { queryParams: { id }});
       }
 }
