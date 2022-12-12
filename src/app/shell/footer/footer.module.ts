b780f183e180f698d0bc9e2d0ec1ee0d46061bc2
mcanam <mcanam910@gmail.com>
2022-12-12 09:03:19 +0700

    add translation

--- src/app/shell/footer/footer.module.ts
+++ src/app/shell/footer/footer.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { FooterComponent } from './footer.component';
 
@@ -9,6 +10,7 @@ import { FooterComponent } from './footer.component';
       ],
       imports: [
             CommonModule,
+            RouterModule,
             SharedModule
       ],
       exports: [
